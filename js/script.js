05cde9c46b42616f3a66a138ba041467d7a68b59
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-04-05 18:52:01 +0200

    end

--- js/script.js
+++ js/script.js
@@ -1,89 +1,96 @@
 
 
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 document.getElementById("button1").addEventListener("click", stampareGriglia1)
-
-
-
 function stampareGriglia1() {
     let container = document.getElementById("container")
     container.innerHTML = ""
     let div = document.createElement("div")
     div.setAttribute('class', 'div-100');
-    let createCols = creaColonne()
-    div.innerHTML = createCols
     container.append(div)
-
+    let createCols = creaColonne()
+    //console.log(creaColonne)
 }
 function creaColonne() {
     let cols = ""
     let numeri = []
+    console.log(numeri)
+    let div = document.getElementsByClassName("div-100");
     for (let i = 1; i <= 100; i++) {
-        cols += `
-          <div class="box">${i}</div>
-          `;
+        let box = document.createElement("div")
+        box.setAttribute('class', 'box')
+        box.innerHTML = `${i}`
+        div[0].append(box)
         numeri.push(i)
         const numeriSingoli = numeri.slice('')
-
+        box.addEventListener("click", changeBg)
     }
-    return cols
+    return numeri
 }
 
-document.getElementById("button2").addEventListener("click", stampareGriglia2)
 
+document.getElementById("button2").addEventListener("click", stampareGriglia2)
 function stampareGriglia2() {
     let container = document.getElementById("container")
     container.innerHTML = ""
     let div = document.createElement("div")
     div.setAttribute('class', 'div-81');
-    let createCols = creaColonne2()
-    div.innerHTML = createCols
     container.append(div)
+    let createCols = creaColonne2()
 }
 function creaColonne2() {
     let cols = ""
     let numeri = []
+    let div = document.getElementsByClassName("div-81");
     for (let i = 1; i <= 81; i++) {
-        cols += `
-          <div class="box">${i}</div>
-          `;
+        let box = document.createElement("div")
+        box.setAttribute('class', 'box')
+        box.innerHTML = `${i}`
+        div[0].append(box)
         numeri.push(i)
         const numeriSingoli = numeri.slice('')
+        box.addEventListener("click", changeBg)
+
     }
-    return cols
+    return numeri
 }
 
-document.getElementById("button3").addEventListener("click", stampareGriglia3)
 
+document.getElementById("button3").addEventListener("click", stampareGriglia3)
 function stampareGriglia3() {
     let container = document.getElementById("container")
     container.innerHTML = ""
     let div = document.createElement("div")
     div.setAttribute('class', 'div-49');
+    container.append(div);
     let createCols = creaColonne3()
-    div.innerHTML = createCols
-    container.append(div)
-
+    generateBomb(numeri)
 }
 function creaColonne3() {
     let cols = ""
     let numeri = []
+    let div = document.getElementsByClassName("div-49");
     for (let i = 1; i <= 49; i++) {
-        cols += `
-             <div class="box">${i}</div>
-             `;
+        let box = document.createElement("div")
+        box.setAttribute('class', 'box')
+        box.innerHTML = `${i}`
+        div[0].append(box)
         numeri.push(i)
         const numeriSingoli = numeri.slice('')
+        box.addEventListener("click", changeBg)
+        generateBomb(numeri)
     }
-    return cols
-}
-function blueBackground(){
-    let nu
-    style.backgroundColor = "#6495ed";
+    return numeri
 }
 
 
+function changeBg() {
+    this.classList.add("blue")
+}
 
-
-//   function colorBlue() {
-//             setAttribute("class", "blue")
-//         }
